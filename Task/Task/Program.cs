5dfc4374d2432e4ef7e92dd065c39ebef357ff8a
Predator <murad.mammedzade11@gmail.com>
2023-08-15 14:08:48 -0700

    Changed

---
+++ Task/Task/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        public class MathHelper
+        {
+            public int PrintValue(int a, int b)
+            {
+                return a + b;
+            }
+            public int PrintValue(bool a)
+            {
+                if (a == true) {return 1;} 
+                else { return 0;}
+            }
+            public void PrintValue(string a,string b)
+            {
+                Console.WriteLine(a + b);
+            }
+            public int PrintValue(int a,int b,int c)
+            {
+                return a * b * c;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var test1 = new MathHelper();
+            Console.WriteLine(test1.PrintValue(1, 2));
+            Console.WriteLine(test1.PrintValue(true));
+            test1.PrintValue("Murad "," Mammedzade");
+            Console.WriteLine(test1.PrintValue(1, 2, 3));
+        }
+    }
+}
